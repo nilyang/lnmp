ac4c3507023c3d406affeca175c4dad2d7cc7a63
nilyang <nil.yang@qq.com>
2015-12-11 09:53:27 +0000

    1.add:区分CentOS和Debian对待MySQL的启动

--- boot-lnmp.sh
+++ boot-lnmp.sh
@@ -1,17 +1,48 @@
 #!/bin/bash
 
+osversion=`cat /etc/issue | awk -F"\n" '{printf("%s",$1)}'|awk '{printf("%s %s\n", $1, $3)}'`
+ostype=CentOS
+if [[ "$osversion" =~ ^CentOS.*$ ]]
+then
+    ostype=CentOS
+fi
+
+if [[ "$osversion" =~ ^Debian.*$ ]]
+then
+    ostype=Debian
+fi
+
 
 pid_phpfpm=`ps aux|grep php-fpm|grep -v "grep"|grep "master process"|awk '{print $2}'`
 pid_nginx=`ps aux|grep nginx|grep -v "grep"|grep "master process"|awk '{print $2}'`
 redis_pid_counts=`ps aux|grep "redis-server"|grep -v "grep"|wc -l`
 
-mysqlcmd=`service mysql`
-if [[ ! ( "$mysqlcmd" =~ ^Usage\:.*$ ) ]]
-then
-   service mysql start
+
+
+if [ $ostype == "Debian" ] ; then
+    mysql_name=mysql
+    mysql_status=`service $mysql_name`
+    if [[ ! ( "$mysql_status" =~ ^(Usage\:.*$ ) ]]
+    then
+       service $mysql_name start
+    else
+       service $mysql_name restart
+    fi
+elif [ $ostype == "CentOS" ] ; then
+    mysql_name=mysqld
+    mysql_status=`service $mysql_name status`
+    if [[ "$mysql_status" =~ ^mysqld.*?pid.*$ ]]
+    then
+       service $mysql_name restart
+    else
+       service $mysql_name start
+    fi
 else
-   service mysql restart
+    echo Error ostype "$osversion" not supported
+    exit
 fi
+
+
 
 if [ "$pid_phpfpm" == "" ]
 then
